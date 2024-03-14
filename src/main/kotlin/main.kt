6c27ec14a3a33456612692edb49d272c611c56f7
Enzo Dragoun Arenhart <enzo.dragoun@rede.ulbra.br>
2024-03-13 21:37:32 -0300

    Aula 02 13-03

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun main()
+{
+    /*val scan = Scanner(System.`in`)
+    var lista: MutableList<Int> = mutableListOf<Int>()
+
+    for(num in 1..5)
+    {
+        println("Digite um número: ")
+        lista.add(scan.nextInt())
+    }
+
+    var soma: Float = 0.0F
+    for(args in lista)
+    {
+        soma += args
+    }
+
+    var media: Float
+
+    media = soma/5
+
+    println(media)
+    */
+
+    val lista = arrayOf("Yellow", "Blue", "Green", "Red")
+
+    
+
+}
